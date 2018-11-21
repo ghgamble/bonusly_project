94ff4fb2d4d740ed2667075b4e7c8f0c5b65ce04
Grace Gamble <grace.h.gamble@gmail.com>
2018-11-20 17:13:15 -0700

    Almost Finished Project

---
+++ main.js
@@ -0,0 +1,34 @@
+$(function(){
+      // Test to make sure JS file is linked up properly with index.html
+      // alert('Successful JS setup!');
+
+      // Save token as independent variable to secure token
+      let token = 'f6e31505b2b20cd6c02104325de3313c';
+
+      var getData = fetch('https://bonus.ly/api/v1/bonuses', {
+          method: 'get',
+          headers: new Headers({
+              'Authorization': 'Bearer '+token,
+              'Content-Type': 'application/x-www-form-urlencoded'
+          })
+      })
+      .then(response => response.json())
+      .then(data => {
+            // Test returned data from API
+            // console.log(data);
+            console.log(data.result);
+            // Loop through all elements in response from API and append each element to an html tag type with an IIFE
+            (function appendBonusList() {
+                  for (var i = 0; i < data.result.length; i++) {
+                        var wrapper = $('<div class="bonus-list-item" />').appendTo('#bonus-list-section');
+                        var recipient = '<h3 class="recipient-name">' + data.result[i].receiver.display_name + '</h3>';
+                        var amount = '<p class="bonus-amount">' + data.result[i].amount + '</p>';
+                        var giverNote = '<p>' + data.result[i].giver.display_name + ': ' + data.result[i].reason_decoded + '</p>';
+                        wrapper.append(recipient, amount, giverNote);
+                  }
+            })();
+      })
+      .catch(function (error) {
+            console.log(error);
+      })
+})
